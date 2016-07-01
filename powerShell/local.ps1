f128df6f6de099e233df1d96bb9e2993420c5651
ao <happy2007xinlan@sina.com>
2016-07-01 17:52:48 +0800

    兼容github项目名称修改 local.ps1文件修改

--- powerShell/local.ps1
+++ powerShell/local.ps1
@@ -11,7 +11,7 @@ $machineArgs['bridgeadapter'] = 'Realtek PCIe GBE 系列控制器';
 
 
 # docker-compose 的变量，不支持powershell对象写法
-$env:compose_dir = '/Users/github/docker/docker/';
+$env:compose_dir = '/Users/github/docker_lamp/docker/';
 $env:compose_volumes_base = '/Users/:/www/';
 $env:compose_volumes_nginx = "${env:compose_dir}nginx/conf/:/usr/src/nginx/conf/";
 $env:compose_volumes_php_conf = "${env:compose_dir}php/conf/conf:/usr/src/php/conf/";
