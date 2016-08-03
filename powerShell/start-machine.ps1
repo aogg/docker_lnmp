9a0f731f6340137506c618bb3f221be971309770
ao <aozhuochao@99cj.com.cn>
2016-08-03 14:54:32 +0800

    兼容v1.12  无需启动docker-machine

--- powerShell/start-machine.ps1
+++ powerShell/start-machine.ps1
@@ -2,20 +2,27 @@
     ${PSScriptRoot} = Split-Path -Parent $MyInvocation.MyCommand.Definition
 }
 
-. ${PSScriptRoot}/create-machine.ps1;
+. ${PSScriptRoot}/common.ps1;
 
-if (-not $?){
-  exit $?;
+if ((de -v) -match 'version [01]\.((0[0-9])|1[01])') { # 这是v1.12
+    . ${rootPath}/create-machine.ps1;
+
+    if (-not $?){
+    exit $?;
+    }
+
+    $status = dm status $hostName;
+    if ($status -eq 'Stopped') {
+    # 开启虚拟机
+    dm start $hostName;
+
+    }
+
+    dm env $hostName | Invoke-Expression;
+
+    echo '虚拟机ip地址为：';
+    dm ip $hostName;
+}else { # v1.12后都为自带的vm
+    
 }
 
-$status = dm status $hostName;
-if ($status -eq 'Stopped') {
-  # 开启虚拟机
-  dm start $hostName;
-
-}
-
-dm env $hostName | Invoke-Expression;
-
-echo '虚拟机ip地址为：';
-dm ip $hostName;
